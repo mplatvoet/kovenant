aafe35b1b13b7e73ce2b825ba578af8ec81701cb
mplatvoet <mplatvoet@gmail.com>
2015-04-08 07:48:47 +0200

    Added some tests

--- src/test/kotlin/jvm/api.kt
+++ src/test/kotlin/jvm/api.kt
@@ -20,9 +20,27 @@ public class APITest {
 
 
     test fun testSuccess() {
-        var called = false
-        Kovenant.async {1+1}.success { called = true }
-        assertEquals(true, called, "succes should be called")
+        var called = 0
+        Kovenant.async {1+1}.success { called++ }
+        assertEquals(1, called, "success should be called once")
+    }
+
+    test fun testFail() {
+        var called = 0
+        Kovenant.async {throw Exception("catch me if you can")}.fail { called++ }
+        assertEquals(1, called, "fail should be called")
+    }
+
+    test fun testAlwaysOnSuccess() {
+        var called = 0
+        Kovenant.async {1+1}.always { called++ }
+        assertEquals(1, called, "always should be called")
+    }
+
+    test fun testAlwaysOnFail() {
+        var called = 0
+        Kovenant.async {throw Exception("catch me if you can")}.always { called++ }
+        assertEquals(1, called, "always should be called")
     }
 }
 
