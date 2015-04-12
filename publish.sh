03b909666afe9d347e4fdc1cacde04a16f85f113
mplatvoet <mplatvoet@gmail.com>
2015-04-12 20:40:32 +0200

    Added documentation facilities

--- publish.sh
+++ publish.sh
@@ -1,1 +1,32 @@
 #!/usr/bin/env bash
+
+echo cleaning build directory
+rm -rf gh-pages
+
+echo cleaning site directory
+rm -rf docs/site
+
+echo cloning git into build directory
+git clone https://github.com/mplatvoet/kovenant.git gh-pages
+cd gh-pages
+git checkout gh-pages
+cd ..
+
+cd docs
+mkdocs build
+cd ..
+cp -rf docs/site/ gh-pages/
+
+cd gh-pages
+git add .
+git status
+git commit -m "auto publish"
+git push
+
+cd ..
+echo cleaning build directory
+rm -rf gh-pages
+
+echo cleaning site directory
+rm -rf site
+echo done
