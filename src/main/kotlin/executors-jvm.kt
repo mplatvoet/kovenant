057d0ed9126ce1d8a504385e9c13aa224ad8705f
mplatvoet <mplatvoet@gmail.com>
2015-04-18 16:29:48 +0200

    Copyright updated

--- src/main/kotlin/executors-jvm.kt
+++ src/main/kotlin/executors-jvm.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2015 Mark Platvoet<mplatvoet@gmail.com>
+ * Copyright (c) 2015 Mark Platvoet<mplatvoet@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -16,7 +16,6 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
 
