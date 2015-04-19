fd572ee8046ea999075f722ee777ee0a2b183384
mplatvoet <mplatvoet@gmail.com>
2015-04-19 17:04:24 +0200

    added some more tests

--- src/test/kotlin/tests/api/promises.kt
+++ src/test/kotlin/tests/api/promises.kt
@@ -37,28 +37,56 @@ class PromiseTest {
         }
     }
 
-    Test fun testSuccess() {
+    Test fun success() {
         var numberOfCalls = 0
         async { 13 } success { numberOfCalls++ }
         assertEquals(1, numberOfCalls, "success should be called once")
     }
 
-    Test fun testAlwaysSuccess() {
+    Test fun alwaysSuccess() {
         var numberOfCalls = 0
         async { 13 } always { numberOfCalls++ }
         assertEquals(1, numberOfCalls, "success should be called once")
     }
 
-    Test fun testFail() {
+    Test fun fail() {
         var numberOfCalls = 0
         async { throw Exception() } fail { numberOfCalls++ }
         assertEquals(1, numberOfCalls, "fail should be called once")
     }
 
-    Test fun testAlwaysFail() {
+    Test fun alwaysFail() {
         var numberOfCalls = 0
         async { throw Exception() } always { numberOfCalls++ }
         assertEquals(1, numberOfCalls, "always should be called once")
+    }
+
+    Test fun multipleSuccesses() {
+        var numberOfCalls = 0
+        async { 13 } success { numberOfCalls++ } success { numberOfCalls++ } success { numberOfCalls++ }
+        assertEquals(3, numberOfCalls, "success should be called 3 times")
+    }
+
+    Test fun multipleFails() {
+        var numberOfCalls = 0
+        async { throw Exception() } fail { numberOfCalls++ } fail { numberOfCalls++ } fail { numberOfCalls++ }
+        assertEquals(3, numberOfCalls, "fail should be called 3 times")
+    }
+
+    Test fun mixedSuccessAndAlways() {
+        var successess = 0
+        var always = 0
+        async { 13 } success { successess++ } always  { always++ } success { successess++ }
+        assertEquals(2, successess, "success should be called 2 times")
+        assertEquals(1, always, "always should be called once")
+    }
+
+    Test fun mixedFailAndAlways() {
+        var fails = 0
+        var always = 0
+        async { throw Exception() } fail { fails++ } always  { always++ } fail { fails++ }
+        assertEquals(2, fails, "fail should be called 2 times")
+        assertEquals(1, always, "always should be called once")
     }
 
 }
---
+++ src/test/kotlin/tests/api/then.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2015 Mark Platvoet<mplatvoet@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * THE SOFTWARE.
+ */
+
+package tests.api.then
+
+import nl.mplatvoet.komponents.kovenant.Kovenant
+import nl.mplatvoet.komponents.kovenant.async
+import nl.mplatvoet.komponents.kovenant.then
+import org.junit.Before
+import org.junit.Test
+import tests.support.ImmediateDispatcher
+import kotlin.test.assertEquals
+
+class ThenTest {
+
+    Before fun setup() {
+        Kovenant.configure {
+            callbackDispatcher = ImmediateDispatcher()
+            workerDispatcher = ImmediateDispatcher()
+        }
+    }
+
+    Test fun thenSuccess() {
+        var result = 0
+        async { 13 } then {it + 2} success { result = it }
+        assertEquals(15, result, "should chain")
+    }
+
+    Test fun thenFail() {
+        var count = 0
+        async { 13 } then {throw Exception()} fail { count++ }
+        assertEquals(1, count, "should report a failure")
+    }
+
+
+}
+
+
+
