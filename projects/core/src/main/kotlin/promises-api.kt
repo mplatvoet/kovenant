6e136e9c5f0c21d3f1ad3d3415f703525cc7e632
mplatvoet <mplatvoet@gmail.com>
2015-05-27 22:15:28 +0200

    documenting in progress

--- projects/core/src/main/kotlin/promises-api.kt
+++ projects/core/src/main/kotlin/promises-api.kt
@@ -191,6 +191,18 @@ public fun deferred<V : Any, E : Any>(context: Context = Kovenant.context): Defe
 public fun async<V : Any>(context: Context = Kovenant.context,
                           body: () -> V): Promise<V, Exception> = concretePromise(context, body)
 
+/**
+ * Asynchronously bind the success value of a [Promise] and returns a new [Promise] with the transformed value.
+ *
+ * Transforms Promise A to Promise B. If Promise A resolves successful then [bind] is executed on the
+ * work [DispatcherContext] of the default `Kovenant.context` and returns Promise B. If [bind] is successful,
+ * meaning now Exception is thrown then Promise B resolves successful, failed otherwise.
+ *
+ * If Promise A fails with error E, Promise B will fail with error E too.
+ *
+ *
+ * @param bind the transform function.
+ */
 public fun <V : Any, R : Any> Promise<V, Exception>.then(bind: (V) -> R): Promise<R, Exception> {
     return concretePromise(context, this, bind)
 }
