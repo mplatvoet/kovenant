75ce5fa758a592c0faa21251fbd17dbbecc1085f
mplatvoet <mplatvoet@gmail.com>
2015-07-25 15:10:22 +0200

    KOV-37 Unwrap method

--- projects/functional/src/main/kotlin/functional.kt
+++ projects/functional/src/main/kotlin/functional.kt
@@ -27,18 +27,28 @@ import java.util.ArrayList
 
 public fun <V : Any, R : Any> Promise<V, Exception>.map(bind: (V) -> R): Promise<R, Exception> = then(bind)
 
+public fun <V : Any, R : Any> Promise<V, Exception>.map(context: Context, bind: (V) -> R): Promise<R, Exception> = then(context, bind)
 
-public fun <V : Any, R : Any> Promise<V, Exception>.flatMap(bind: (V) -> Promise<R, Exception>): Promise<R, Exception> {
+
+public fun <V : Any, R : Any> Promise<V, Exception>.flatMap(bind: (V) -> Promise<R, Exception>): Promise<R, Exception> = flatMap(context, bind)
+
+public fun <V : Any, R : Any> Promise<V, Exception>.flatMap(context: Context, bind: (V) -> Promise<R, Exception>): Promise<R, Exception> {
     val deferred = deferred<R, Exception>(context)
     success {
         value ->
-        context.workerContext.offer {
-            val p = bind(value)
-            p success {
-                deferred resolve it
-            }
-            p fail {
-                deferred reject it
+        context.workerContext offer {
+            try {
+                val p = bind(value)
+
+                p success {
+                    deferred resolve it
+                }
+                p fail {
+                    deferred reject it
+                }
+            } catch (e: Exception) {
+                //just like map/then consider bind exception as rejection
+                deferred reject e
             }
         }
     }
@@ -49,9 +59,9 @@ public fun <V : Any, R : Any> Promise<V, Exception>.flatMap(bind: (V) -> Promise
     return deferred.promise
 }
 
-fun <V, R> Sequence<V>.aforEach(context: Context = Kovenant.context, bind: (V) -> R): Promise<List<R>, Exception> {
+public fun <V, R> Sequence<V>.aforEach(context: Context = Kovenant.context, bind: (V) -> R): Promise<List<R>, Exception> {
     val deferred = deferred<List<R>, Exception>(context)
-    context.workerContext.offer {
+    context.workerContext offer {
         val promises = ArrayList<Promise<R, Exception>>()
         forEach {
             value ->
@@ -67,5 +77,51 @@ fun <V, R> Sequence<V>.aforEach(context: Context = Kovenant.context, bind: (V) -
         }
     }
 
+    return deferred.promise
+}
+
+public fun <V, E> Promise<Promise<V, E>, E>.unwrap(context: Context = this.context): Promise<V, E> {
+    if (isDone()) when {
+        isSuccess() -> return get().withContext(context)
+        isFailure() -> return Promise.ofFail(getError(), context)
+    }
+
+    val deferred = deferred<V, E>(context)
+    success {
+        nested ->
+        nested success { deferred resolve it }
+        nested fail { deferred reject it }
+    } fail {
+        deferred reject it
+    }
+    return deferred.promise
+}
+
+
+/**
+ * Returns a Promise operating on the provided Context
+ *
+ * This function might return the same instance of the Promise or a new one depending whether the
+ * context of the Promise and the provided promise match.
+ *
+ *
+ * @param context The context on which the returned promise should operate
+ * @return the same promise is the context matches, a new promise otherwise with the provided context otherwise
+ */
+public fun <V, E>Promise<V, E>.withContext(context: Context): Promise<V, E> {
+    // Already same context, just return self
+    if (this.context == context) return this
+
+    // avoid using deferred and callbacks if this promise
+    // is already resolved
+    if (isDone()) when {
+        isSuccess() -> return Promise.ofSuccess(get(), context)
+        isFailure() -> return Promise.ofFail(getError(), context)
+    }
+
+    //okay, the hard way
+    val deferred = deferred<V, E>(context)
+    success { deferred resolve it }
+    fail { deferred reject it }
     return deferred.promise
 }
