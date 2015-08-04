a4d1b906aaa8f9bc3fe3648aee66917daf7fd4c1
mplatvoet <mplatvoet@gmail.com>
2015-08-04 09:50:05 +0200

    validation for get added

---
+++ projects/core/src/test/kotlin/validate/get.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015 Mark Platvoet<mplatvoet@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package validate.get
+
+import nl.komponents.kovenant.async
+import support.fib
+import java.util.*
+
+
+fun main(args: Array<String>) {
+    val attempts = 100000
+    val results = ArrayList<Int>(attempts)
+
+    for (i in 1..10000) {
+        results add async { fib(i % 23) }.get()
+    }
+
+    println("got ${results.size()} results")
+}
