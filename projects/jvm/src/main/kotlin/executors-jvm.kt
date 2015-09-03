23e5a5548faf171d51ef923bc60cb33aff578946
mplatvoet <mplatvoet@gmail.com>
2015-09-03 21:26:24 +0200

    Updated to latest snapshot

--- projects/jvm/src/main/kotlin/executors-jvm.kt
+++ projects/jvm/src/main/kotlin/executors-jvm.kt
@@ -260,7 +260,7 @@ private data class DispatcherExecutorService(private val dispatcher: Dispatcher)
             throw e
         }
 
-        val finished = finishedFutures.entrySet() sortBy { entry -> entry.key } map { entry -> entry.value }
+        val finished = finishedFutures.entrySet() sortedBy { entry -> entry.key } map { entry -> entry.value }
 
         //Can happen when we are using a timeout on the latch
         if (finished.size() < allFutures.size()) {
