c63315b646b7863a9416bb08912e16379b39e1e5
mplatvoet <mplatvoet@gmail.com>
2016-02-28 08:01:54 +0100

    Added Unit Tests

--- projects/rx/src/test/kotlin/examples/observables.kt
+++ projects/rx/src/test/kotlin/examples/observables.kt
@@ -21,15 +21,39 @@
 
 package nl.komponents.kovenant.rx.examples
 
-import nl.komponents.kovenant.rx.toObservable
-import nl.komponents.kovenant.task
-import rx.schedulers.Schedulers
+import nl.komponents.kovenant.Kovenant
+import nl.komponents.kovenant.Promise
+import nl.komponents.kovenant.rx.EmitStrategy
+import nl.komponents.kovenant.rx.toListPromise
+import nl.komponents.kovenant.rx.toPromise
+import nl.komponents.kovenant.testMode
+import rx.Observable
+
 
 fun main(args: Array<String>) {
-    val observable = task { 42 }.toObservable()
+    Kovenant.testMode { }
 
-    observable.subscribeOn(Schedulers.computation()).subscribe {
-        println(it)
-    }
+    //Default behaviour, take first element, exception on empty Observable
+    Observable.from(arrayOf(1, 2, 3, 4, 5)).toPromise().printResult()
+    Observable.from(arrayOf<Int>()).toPromise().printResult()
+
+    //Take last element
+    Observable.from(arrayOf(1, 2, 3, 4, 5)).toPromise(strategy = EmitStrategy.LAST).printResult()
+
+    //default value on empty Observable
+    Observable.from(arrayOf<Int>()).toPromise(42).printResult()
+
+    //default value factory on empty Observable
+    Observable.from(arrayOf<Int>()).toPromise() { 4 * 16 }.printResult()
+
+
+    //Put everything in a list
+    Observable.from(arrayOf(1, 2, 3, 4, 5)).toListPromise().printResult()
+    Observable.from(arrayOf<Int>()).toListPromise().printResult()
 }
 
+fun <V, E> Promise<V, E>.printResult() = success {
+    println("Success value = $it")
+} fail {
+    println("Fail value = $it")
+}
