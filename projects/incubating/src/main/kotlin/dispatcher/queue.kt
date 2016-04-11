c746dca8d9854886a313cafdeb87d0152191d5a4
mplatvoet <mplatvoet@gmail.com>
2016-04-11 22:36:05 +0200

    Work in Progress. Creating a sticky thread dispatcher.

--- projects/incubating/src/main/kotlin/dispatcher/queue.kt
+++ projects/incubating/src/main/kotlin/dispatcher/queue.kt
@@ -21,20 +21,61 @@
 
 package nl.komponents.kovenant.incubating.dispatcher
 
-class StickyThreadQueue<E : Any> {
-    //    companion object {
-    //        private val headRefUpdater = AtomicReferenceFieldUpdater.newUpdater(StickyThreadQueue::class.java, QueueNode::class.java, "_head")
-    //    }
+import nl.komponents.kovenant.unsafe.UnsafeAtomicReferenceFieldUpdater
+import nl.komponents.kovenant.unsafe.hasUnsafe
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater
 
+class StickyThreadBlockingQueue<E : Any> {
     private @Volatile var head: QueueNode<E> = QueueNode()
 
-    fun add(element: E, thread: Thread?) {
+    fun pop(thread: Thread = Thread.currentThread()): E {
+        while (true) {
+            var current = head
+            while (true) {
+                //no work for this thread, thus break out
+                current = current.next ?: break
+
+                if (current.thread == thread) {
+                    if (current.isRead()) {
+                        //TODO shift head if necessary
+
+                        continue
+                    }
+                    return popNode(current)
+
+                }
+
+                if (current.thread == null && current.trySetThread(thread)) {
+                    return popNode(current)
+                }
+
+            }
+            TODO("should block here")
+        }
+    }
+
+    //assumes this node contains the proper thread
+    private fun popNode(node: QueueNode<E>): E {
+        val value = node.unsafeGetValue()
+        if (head.next == node) {
+            head = node
+        } else {
+            node.markAsRead()
+        }
+        return value
+    }
+
+
+    fun push(element: E, thread: Thread?) {
         val node = detachedNodeFor(element, thread)
         appendNode(node)
     }
 
     private fun appendNode(node: QueueNode<E>) {
-
+        do {
+            //TODO, optimize tail finding. e.g. use second node for cached tail
+            val tail = head.findTail()
+        } while (!tail.trySetNext(node))
     }
 
     private fun detachedNodeFor(element: E, thread: Thread?) = QueueNode<E>().apply {
@@ -45,11 +86,40 @@ class StickyThreadQueue<E : Any> {
 
 
 class QueueNode<E : Any> {
-    private @Volatile var _value: E? = null
+    companion object {
+        private val nextUpdater: AtomicReferenceFieldUpdater<QueueNode<*>, QueueNode<*>>
+        private val threadUpdater: AtomicReferenceFieldUpdater<QueueNode<*>, Thread>
+
+        private val read = Any()
+
+        init {
+            if (hasUnsafe()) {
+                nextUpdater = UnsafeAtomicReferenceFieldUpdater(QueueNode::class, "_next")
+                threadUpdater = UnsafeAtomicReferenceFieldUpdater(QueueNode::class, "_thread")
+            } else {
+                nextUpdater = AtomicReferenceFieldUpdater.newUpdater(QueueNode::class.java, QueueNode::class.java, "_next")
+                threadUpdater = AtomicReferenceFieldUpdater.newUpdater(QueueNode::class.java, Thread::class.java, "_thread")
+            }
+        }
+    }
+
+
+    private @Volatile var _value: Any? = null
     private @Volatile var _next: QueueNode<E>? = null
     private @Volatile var _thread: Thread? = null
 
     val next: QueueNode<E>? get() = _next
+
+    val thread: Thread? get() = _thread
+
+    fun isRead() = _value == read
+
+    fun markAsRead() {
+        _value = read
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun unsafeGetValue(): E = _value as E
 
     fun setValueLazy(newValue: E) {
         _value = newValue
@@ -58,19 +128,13 @@ class QueueNode<E : Any> {
     fun setThreadLazy(newThread: Thread?) {
         _thread = newThread
     }
+
+    fun trySetThread(thread: Thread) = threadUpdater.compareAndSet(this, null, thread)
+
+    fun trySetNext(node: QueueNode<E>) = nextUpdater.compareAndSet(this, null, node)
 }
 
-
-inline fun <E : Any> QueueNode<E>.stream(body: (QueueNode<E>) -> Unit) {
+private fun <E : Any> QueueNode<E>.findTail(): QueueNode<E> {
     var current = this
-    while (true) {
-        body(current)
-        val next = current.next
-        if (next != null) {
-            current = next
-        } else {
-            break
-        }
-    }
+    while (true) current = current.next ?: return current
 }
-
