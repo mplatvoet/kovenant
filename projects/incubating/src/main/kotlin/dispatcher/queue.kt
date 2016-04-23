f6e49892fad4f17c1b413fd17935b4175bd5de74
mplatvoet <mplatvoet@gmail.com>
2016-04-23 22:11:53 +0200

    Work in Progress. Queue works: Creating a sticky thread dispatcher.

--- projects/incubating/src/main/kotlin/dispatcher/queue.kt
+++ projects/incubating/src/main/kotlin/dispatcher/queue.kt
@@ -21,13 +21,30 @@
 
 package nl.komponents.kovenant.incubating.dispatcher
 
+import nl.komponents.kovenant.FailedException
 import nl.komponents.kovenant.unsafe.UnsafeAtomicReferenceFieldUpdater
 import nl.komponents.kovenant.unsafe.hasUnsafe
+import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater
 
-class StickyThreadBlockingQueue<E : Any> {
-    private @Volatile var head: QueueNode<E> = QueueNode()
 
+/**
+ *
+ * Problems with current implementation direction:
+ * * Dead threads don't clean nodes. Weakrefs can solve this but with excessive overhead.
+ * * Appending is slow. No cached tail node yet
+ * * blocked threads might not wake properly when fighting for an unbound node, verify
+ *
+ */
+
+class StickyThreadBlockingQueue<E : Any> {
+    private val head: QueueNode<E> = QueueNode()
+    private val waitingThreads = AtomicInteger(0)
+
+    @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
+    private val mutex: Object get() = waitingThreads as Object
+
+    @Throws(InterruptedException::class)
     fun pop(thread: Thread = Thread.currentThread()): E {
         while (true) {
             var current = head
@@ -37,8 +54,9 @@ class StickyThreadBlockingQueue<E : Any> {
 
                 if (current.thread == thread) {
                     if (current.isRead()) {
-                        //TODO shift head if necessary
-
+                        if (current == head.next) {
+                            head.setNextLazy(current.next)
+                        }
                         continue
                     }
                     return popNode(current)
@@ -50,7 +68,40 @@ class StickyThreadBlockingQueue<E : Any> {
                 }
 
             }
-            TODO("should block here")
+            waitingThreads.incrementAndGet()
+            try {
+                synchronized(mutex) {
+                    while (!containsNodeForThread(thread)) {
+                        try {
+                            mutex.wait()
+                        } catch(e: InterruptedException) {
+                            throw FailedException(e)
+                        }
+                    }
+                }
+            } finally {
+                waitingThreads.decrementAndGet()
+            }
+        }
+    }
+
+    fun isEmpty(): Boolean = head.next === null
+
+    private fun notifyBlockedThreads() {
+        if (waitingThreads.get() > 0) {
+            synchronized(mutex) {
+                mutex.notifyAll()
+            }
+        }
+    }
+
+    private fun containsNodeForThread(thread: Thread): Boolean {
+        var current = head
+        while (true) {
+            //no work for this thread, thus break out
+            current = current.next ?: return false
+
+            if (current.thread == null || current.thread === thread ) return true
         }
     }
 
@@ -58,7 +109,7 @@ class StickyThreadBlockingQueue<E : Any> {
     private fun popNode(node: QueueNode<E>): E {
         val value = node.unsafeGetValue()
         if (head.next == node) {
-            head = node
+            head.setNextLazy(node.next)
         } else {
             node.markAsRead()
         }
@@ -76,6 +127,7 @@ class StickyThreadBlockingQueue<E : Any> {
             //TODO, optimize tail finding. e.g. use second node for cached tail
             val tail = head.findTail()
         } while (!tail.trySetNext(node))
+        notifyBlockedThreads()
     }
 
     private fun detachedNodeFor(element: E, thread: Thread?) = QueueNode<E>().apply {
@@ -110,9 +162,10 @@ class QueueNode<E : Any> {
 
     val next: QueueNode<E>? get() = _next
 
+    //TODO, make this a WeakRef?
     val thread: Thread? get() = _thread
 
-    fun isRead() = _value == read
+    fun isRead() = _value === read
 
     fun markAsRead() {
         _value = read
@@ -123,6 +176,10 @@ class QueueNode<E : Any> {
 
     fun setValueLazy(newValue: E) {
         _value = newValue
+    }
+
+    fun setNextLazy(node: QueueNode<E>?) {
+        _next = node
     }
 
     fun setThreadLazy(newThread: Thread?) {
