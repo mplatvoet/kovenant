aae6ccb36a63d7edc108143d58417b99dbb5582d
adambl4 <dreadambl4@gmail.com>
2016-12-28 14:15:22 +0300

    Bring Kotlin coroutines model up to date

--- projects/core/src/test/kotlin/examples/coroutines.kt
+++ projects/core/src/test/kotlin/examples/coroutines.kt
@@ -2,15 +2,14 @@ package examples.coroutines
 
 import nl.komponents.kovenant.Promise
 import nl.komponents.kovenant.async
+import nl.komponents.kovenant.await
 
 fun main(args: Array<String>) {
-    val promise = async<Int> {
+    val promise = async {
         await(Promise.of(13)) + 15
     }
 
-    promise success {
-        println(it)
-    }
+    promise success ::println
 }
 
 
